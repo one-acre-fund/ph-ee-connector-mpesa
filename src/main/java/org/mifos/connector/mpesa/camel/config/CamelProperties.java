9e8ae2afdb8324cb7403ea1b04e640b32d7a3883
Sabin Sheja <sabin.sheja@oneacrefund.org>
2026-06-19 16:49:35 +0200

    fix: result code when not provided

--- src/main/java/org/mifos/connector/mpesa/camel/config/CamelProperties.java
+++ src/main/java/org/mifos/connector/mpesa/camel/config/CamelProperties.java
@@ -38,6 +38,7 @@ public class CamelProperties {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String RECONCILED = "reconciled";
     public static final String MPESA_TXN_ID = "mpesaTxnId";
+    public static final String MPESA_RECEIPT_NUMBER = "MpesaReceiptNumber";
     public static final String CLIENT_CORRELATION_ID = "X-CorrelationID";
     public static final String ACCOUNT_HOLDING_INSTITUTION_ID = "accountHoldingInstitutionId";
 }
